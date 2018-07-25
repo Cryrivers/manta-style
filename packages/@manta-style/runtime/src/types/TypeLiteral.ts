01d4959a1b45bcda36b39339a1714dd825268895
Wang Zhongliang <zwang@rebelworks.io>
2018-07-26 00:07:15 +0800

    WIP: support index signature and "in" keyword again in refactored types

--- packages/@manta-style/runtime/src/types/TypeLiteral.ts
+++ packages/@manta-style/runtime/src/types/TypeLiteral.ts
@@ -59,65 +59,83 @@ export default class TypeLiteral extends Type {
         typeLiteral.property(
           property.name,
           property.type.deriveLiteral(property.annotations),
-          true,
+          false,
           property.annotations
         );
       }
     }
 
     // Enumerate IndexSignatures
-    // for (const computedProperty of this.computedProperties) {
-    //   const jsDocForKeys: Annotation[] = computedProperty.annotations
-    //     .filter(item => item.key === "key")
-    //     .map(item => ({ ...item, key: "example" }));
-    //   const jsDocForValues: Annotation[] = computedProperty.annotations.filter(
-    //     item => item.key !== "key"
-    //   );
+    for (const computedProperty of this.computedProperties) {
+      const jsDocForKeys: Annotation[] = computedProperty.annotations
+        .filter(item => item.key === "key")
+        .map(item => ({ ...item, key: "example" }));
+      const jsDocForValues: Annotation[] = computedProperty.annotations.filter(
+        item => item.key !== "key"
+      );
 
-    //   if (
-    //     computedProperty.operator === ComputedPropertyOperator.INDEX_SIGNATURE
-    //   ) {
-    //     if (jsDocForKeys.length > 0) {
-    //       for (let i = 0; i < jsDocForKeys.length; i++) {
-    //         const key = computedProperty.keyType.mock([jsDocForKeys[i]]);
-    //         const value = computedProperty.type.mock(jsDocForValues);
-    //         obj[key] = value;
-    //       }
-    //     } else {
-    //       const key = computedProperty.keyType.mock([
-    //         {
-    //           key: "example",
-    //           value: "This is a key. Customize it with JSDoc tag @key"
-    //         }
-    //       ]);
-    //       const value = computedProperty.type.mock(jsDocForValues);
-    //       obj[key] = value;
-    //     }
-    //   } else if (
-    //     computedProperty.operator === ComputedPropertyOperator.IN_KEYWORD
-    //   ) {
-    //     // FIXME: I feel this implementation is not correct
-    //     const { keyType, name } = computedProperty;
-    //     const subobj = (obj[name] = {} as AnyObject);
-    //     const actualType = resolveReferencedType(keyType);
-    //     if (actualType instanceof KeyOfKeyword) {
-    //       for (const key of actualType.getKeys()) {
-    //         subobj[key] = computedProperty.type.mock(
-    //           computedProperty.annotations
-    //         );
-    //       }
-    //     } else if (actualType instanceof UnionType) {
-    //       for (const key of actualType.mockAll()) {
-    //         subobj[key] = computedProperty.type.mock(
-    //           computedProperty.annotations
-    //         );
-    //       }
-    //     } else {
-    //       console.log(actualType);
-    //       throw new Error(`Unsupported Type after keyword "in"`);
-    //     }
-    //   }
-    // }
+      if (
+        computedProperty.operator === ComputedPropertyOperator.INDEX_SIGNATURE
+      ) {
+        if (jsDocForKeys.length > 0) {
+          for (let i = 0; i < jsDocForKeys.length; i++) {
+            const chance = computedProperty.questionMark ? Math.random() : 1;
+            const literal = computedProperty.type.deriveLiteral(jsDocForValues);
+            // TODO: Remove the assumption of string index signature.
+            // support number in future
+            if (chance > 0.5) {
+              typeLiteral.property(
+                jsDocForKeys[i].value,
+                literal,
+                false,
+                jsDocForValues
+              );
+            }
+          }
+        } else {
+          typeLiteral.property(
+            "This is a key. Customize it with JSDoc tag @key",
+            computedProperty.type.deriveLiteral(jsDocForValues),
+            false,
+            jsDocForValues
+          );
+        }
+      } else if (
+        computedProperty.operator === ComputedPropertyOperator.IN_KEYWORD
+      ) {
+        // TODO: Need to review again, didnt deal with 2 question marks perfectly
+        const { keyType, name } = computedProperty;
+        const subTypeLiteral = new TypeLiteral();
+        // TODO: Correct annotation
+        typeLiteral.property(name, subTypeLiteral, false, []);
+        const actualType = resolveReferencedType(keyType);
+        if (
+          actualType instanceof KeyOfKeyword ||
+          actualType instanceof UnionType
+        ) {
+          const keys =
+            actualType instanceof KeyOfKeyword
+              ? actualType.getKeys()
+              : actualType.getTypes().map(type => type.deriveLiteral().mock());
+          for (const key of keys) {
+            const chance = computedProperty.questionMark ? Math.random() : 1;
+            if (chance > 0.5) {
+              subTypeLiteral.property(
+                key,
+                computedProperty.type.deriveLiteral(
+                  computedProperty.annotations
+                ),
+                false,
+                computedProperty.annotations
+              );
+            }
+          }
+        } else {
+          console.log(actualType);
+          throw new Error(`Unsupported Type after keyword "in"`);
+        }
+      }
+    }
     return typeLiteral;
   }
   public mock() {
