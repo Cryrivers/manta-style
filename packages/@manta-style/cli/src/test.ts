f352b29229f6255af9675c54353c4aaadaa752f8
Wang Zhongliang <zwang@rebelworks.io>
2018-08-16 23:27:09 +0800

    Fix the crashing issue

--- packages/@manta-style/cli/src/test.ts
+++ packages/@manta-style/cli/src/test.ts
@@ -1,7 +1,11 @@
 export type GET = {
   /**
+   * @proxy https://jsonplaceholder.typicode.com
+   */
+  '/todos/1': { haha: number };
+  /**
    * @proxy https://www.google.com
    */
-  '/test1': { haha: number };
-  '/test2': { haha: number };
+  '/errorExample': { haha: number };
+  '/test': { haha: number };
 };
