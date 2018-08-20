0a338b7c5a5facf0cec46dd81224ba0ddf15b25b
Wang Zhongliang <zwang@rebelworks.io>
2018-08-20 22:19:23 +0800

    remove TypeReference

--- packages/@manta-style/runtime/__tests__/types/TypeLiteral.ts
+++ packages/@manta-style/runtime/__tests__/types/TypeLiteral.ts
@@ -5,21 +5,17 @@ describe('TypeLiteral Test', () => {
     MS.clearType();
   });
   test('TypeLiteral can mock', () => {
-    const GenericTypeLiteral = () =>
-      MS.TypeAliasDeclaration(
-        'GenericTypeLiteral',
-        (type) => {
-          const T = type.TypeParameter('T');
-          return MS.TypeLiteral((currentType) => {
-            currentType.property('test', T, false, []);
-          });
-        },
-        [],
-      );
-    MS.registerType('GenericTypeLiteral', GenericTypeLiteral);
-    const TestObject = MS.referenceType('GenericTypeLiteral').argumentTypes([
-      MS.NumberKeyword,
-    ]);
+    const GenericTypeLiteral = MS.TypeAliasDeclaration(
+      'GenericTypeLiteral',
+      (type) => {
+        const T = type.TypeParameter('T');
+        return MS.TypeLiteral((currentType) => {
+          currentType.property('test', T, false, []);
+        });
+      },
+      [],
+    );
+    const TestObject = GenericTypeLiteral.argumentTypes([MS.NumberKeyword]);
     const result = TestObject.deriveLiteral([]).mock();
     expect(typeof result.test === 'number').toBe(true);
   });
