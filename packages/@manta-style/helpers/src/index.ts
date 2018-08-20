e9e9ac50c8049c8b542152de04d50d10e1ee8eb3
Wang Zhongliang <zwang@rebelworks.io>
2018-08-20 22:39:50 +0800

    Make QueryType support new LazyTypeDeclaration

--- packages/@manta-style/helpers/src/index.ts
+++ packages/@manta-style/helpers/src/index.ts
@@ -1,13 +1,11 @@
 import QueryType from './types/QueryType';
 import MantaStyle from '@manta-style/runtime';
 
-export const Query = () =>
-  MantaStyle.TypeAliasDeclaration(
-    'Query',
-    (currentType) => {
-      const T = currentType.TypeParameter('T');
-      return new QueryType(T);
-    },
-    [],
-  );
-MantaStyle.registerType('Query', Query);
+export const Query = MantaStyle.TypeAliasDeclaration(
+  'Query',
+  (typeFactory) => {
+    const T = typeFactory.TypeParameter('T');
+    return new QueryType(T);
+  },
+  [],
+);
