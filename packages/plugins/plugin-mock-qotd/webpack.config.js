31b10592fa140ae1f68fe69cf32b88e9308e7e65
Tan Li Hau <lhtan93@gmail.com>
2018-08-30 11:28:51 +0800

    QotD Mock Plugin

---
+++ packages/plugins/plugin-mock-qotd/webpack.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+    mode: 'production',
+    entry: __dirname + '/src/index.ts',
+    devtool: 'source-map',
+    target: 'node',
+    output: {
+        path: __dirname + '/lib',
+        filename: 'index.js',
+        library: 'MantaStylePluginMockQotD',
+        libraryTarget: 'umd',
+        // See https://github.com/webpack/webpack/issues/6522
+        globalObject: "typeof self !== 'undefined' ? self : this",
+        umdNamedDefine: true,
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.json'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: ['awesome-typescript-loader'],
+            },
+            {
+                test: /\.js$/,
+                use: ['source-map-loader'],
+                enforce: 'pre',
+            },
+        ],
+    },
+    performance: {
+        hints: false,
+    },
+};
