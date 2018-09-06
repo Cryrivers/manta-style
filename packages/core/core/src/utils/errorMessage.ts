9703c9aea26c93e7f39107b48fb13e891326d13a
Wang Zhongliang <zwang@rebelworks.io>
2018-09-06 15:05:16 +0800

    WIP: ErrorCode module

---
+++ packages/core/core/src/utils/errorMessage.ts
@@ -0,0 +1,48 @@
+const enum ErrorCode {
+  UNSUPPORTED_EXTENSION = 0,
+  BUILDER_PLUGIN_ERROR = 1,
+  MOCK_PLUGIN_ERROR = 2,
+}
+
+type ErrorDetail = {
+  [ErrorCode.UNSUPPORTED_EXTENSION]: (extension: string) => string;
+  [ErrorCode.BUILDER_PLUGIN_ERROR]: (
+    builderName: string,
+    errorMessage: string,
+  ) => string;
+  [ErrorCode.MOCK_PLUGIN_ERROR]: (
+    mockerName: string,
+    errorMessage: string,
+  ) => string;
+};
+
+function errmsg(message: TemplateStringsArray, ...keys: string[]): string {
+  let finalMessage = '';
+  const normalizedMessage = message.map((str) => str.replace('"', "'"));
+  const { length } = normalizedMessage;
+  for (let i = 0; i < length; i++) {
+    finalMessage += normalizedMessage[i];
+    if (i < length - 1) {
+      finalMessage += '"' + keys[i] + '"';
+    }
+  }
+  return finalMessage;
+}
+
+const errorMessages: ErrorDetail = {
+  [ErrorCode.UNSUPPORTED_EXTENSION]: (extension: string) =>
+    errmsg`Extension ${extension} is not handled by any builder plugins.`,
+  [ErrorCode.BUILDER_PLUGIN_ERROR]: (
+    builderName: string,
+    errorMessage: string,
+  ) => ``,
+  [ErrorCode.MOCK_PLUGIN_ERROR]: (mockerName: string, errorMessage: string) =>
+    ``,
+};
+
+export function generateErrorMessage<T extends ErrorCode>(
+  errorCode: T,
+  ...args: ErrorDetail[T] extends (...args: infer R) => string ? R : []
+) {
+  return `MS-${errorCode}: ${errorMessages[errorCode](...args)}`;
+}
