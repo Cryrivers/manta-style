0e5fc2f5e98f9cfffd1737ae9b7ce4506b4a3433
Wang Zhongliang <wangz@garena.com>
2018-12-11 15:04:14 +0800

    Remove old context

--- packages/core/runtime/__tests__/utils/assignable.ts
+++ packages/core/runtime/__tests__/utils/assignable.ts
@@ -1,35 +1,32 @@
 import { isAssignable } from '../../src/utils/assignable';
 import MS from '../../src';
-import { PluginSystem } from '@manta-style/core';
 
 describe('Assignablity Test', () => {
-  const context = { query: {}, param: {}, plugins: PluginSystem.default() };
-
   test('String Keyword and literals', () => {
     const LiteralA = MS.Literal('Hello');
     const LiteralB = MS.Literal('Hello');
     const LiteralC = MS.Literal('World');
-    expect(isAssignable(LiteralA, LiteralB, context)).toBe(true);
-    expect(isAssignable(LiteralA, LiteralC, context)).toBe(false);
-    expect(isAssignable(LiteralA, MS.StringKeyword, context)).toBe(true);
-    expect(isAssignable(LiteralC, MS.StringKeyword, context)).toBe(true);
+    expect(isAssignable(LiteralA, LiteralB)).toBe(true);
+    expect(isAssignable(LiteralA, LiteralC)).toBe(false);
+    expect(isAssignable(LiteralA, MS.StringKeyword)).toBe(true);
+    expect(isAssignable(LiteralC, MS.StringKeyword)).toBe(true);
   });
   test('Number Keyword and literals', () => {
     const LiteralA = MS.Literal(0);
     const LiteralB = MS.Literal(0);
     const LiteralC = MS.Literal(1);
-    expect(isAssignable(LiteralA, LiteralB, context)).toBe(true);
-    expect(isAssignable(LiteralA, LiteralC, context)).toBe(false);
-    expect(isAssignable(LiteralA, MS.NumberKeyword, context)).toBe(true);
-    expect(isAssignable(LiteralC, MS.NumberKeyword, context)).toBe(true);
+    expect(isAssignable(LiteralA, LiteralB)).toBe(true);
+    expect(isAssignable(LiteralA, LiteralC)).toBe(false);
+    expect(isAssignable(LiteralA, MS.NumberKeyword)).toBe(true);
+    expect(isAssignable(LiteralC, MS.NumberKeyword)).toBe(true);
   });
   test('Boolean Keyword and literals', () => {
     const LiteralA = MS.Literal(true);
     const LiteralB = MS.Literal(true);
     const LiteralC = MS.Literal(false);
-    expect(isAssignable(LiteralA, LiteralB, context)).toBe(true);
-    expect(isAssignable(LiteralA, LiteralC, context)).toBe(false);
-    expect(isAssignable(LiteralA, MS.BooleanKeyword, context)).toBe(true);
-    expect(isAssignable(LiteralC, MS.BooleanKeyword, context)).toBe(true);
+    expect(isAssignable(LiteralA, LiteralB)).toBe(true);
+    expect(isAssignable(LiteralA, LiteralC)).toBe(false);
+    expect(isAssignable(LiteralA, MS.BooleanKeyword)).toBe(true);
+    expect(isAssignable(LiteralC, MS.BooleanKeyword)).toBe(true);
   });
 });
